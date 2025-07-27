a2009737a73e154b8a3ce045f3c2d7dad32a0f67
Nikita <knopaaaa3101@gmail.com>
2025-07-27 23:13:06 +0300

    02-react-cafe

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
 // @ts-ignore
 import 'modern-normalize';
 import './index.css';
